ffaae63ebf8237f1127909b592ef2275e174abc9
JStockwell <jstockwellmendoza@gmail.com>
2023-12-01 10:33:21 +0100

    feat: Version Exclusives and table filters

--- flask/stockies_encounter_dex/db_view.py
+++ flask/stockies_encounter_dex/db_view.py
@@ -8,12 +8,12 @@ from stockies_encounter_dex.db import get_db
 
 bp = Blueprint('db_view', __name__)
 
-@bp.route('/')
-def index():
+@bp.route('/encounters')
+def encounters():
     db = get_db()
 
     encounters = db.execute(
-        'SELECT pokemon.name, game_id, location_id, chance, max_level, min_level, method, condition'
+        'SELECT pokemon_id, pokemon.name, game_id, location_id, chance, max_level, min_level, method, condition'
         ' FROM encounters JOIN pokemon ON pokemon_id = pokemon.id'
         ' ORDER BY pokemon_id ASC'
     ).fetchall()
@@ -22,4 +22,26 @@ def index():
         'SELECT * FROM games'
     ).fetchall()
 
-    return render_template('db_view/index.html', encounters=encounters, games=games)
+    data = [encounters, games]
+    return render_template('db_view/encounters.html', data=data)
+
+@bp.route('/version-exclusives')
+def version_exclusives():
+    db = get_db()
+
+    encounters = db.execute(
+        'SELECT *'
+        ' FROM version_exclusive_pokemon'
+        ' ORDER BY pokemon_id ASC'
+    ).fetchall()
+
+    games = db.execute(
+        'SELECT * FROM games'
+    ).fetchall()
+
+    data = [encounters, games]
+    return render_template('db_view/version_exclusives.html', data=data)
+
+@bp.route('/')
+def index():
+    return render_template('db_view/index.html')
